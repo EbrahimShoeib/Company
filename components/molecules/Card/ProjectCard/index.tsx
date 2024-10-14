8863af9d4cfe37d8109030f56f55e4571bfbef4a
shoeib <ibrahimshoeib255@gmail.com>
2024-10-14 08:36:43 -0700

    chore

--- components/molecules/Card/ProjectCard/index.tsx
+++ components/molecules/Card/ProjectCard/index.tsx
@@ -1,5 +1,5 @@
 // import Button from 'components/atoms/Button'
-import ButtonLink from 'components/atoms/Button/ButtonLink'
+import ButtonLink from 'components/atoms/Button/ButtonLink';
 // import LazyCard, { LazyCardProps } from 'components/atoms/LazyCard'
 // import Text from 'components/atoms/Text'
 // interface ProjectCardProps extends LazyCardProps {
@@ -39,42 +39,39 @@ import ButtonLink from 'components/atoms/Button/ButtonLink'
 // }
 
 // export default ProjectCard
-import Button from 'components/atoms/Button'
+import Button from 'components/atoms/Button';
 
 import React from 'react';
 
-type TypeProps={
-  title:string,
-  description:string,
-  image:string,
-  alt:string
-}
-const ProjectCard = (props:TypeProps) => {
-  return (
-    <div className='flex justify-center  p-3   '>
-      <div className="  max-w-sm rounded-3xl overflow-hidden shadow-lg">
-  <img className="w-full" src={props.image} alt={props.alt} />
-  <div className="flex flex-col place-items-center text-center mt-10">
-        <div className="mb-2.5">
-    <div className="font-bold text-xl mb-2 text-primary">{props.title}</div>
-    <p className="text-white text-base">
-{props.description}    </p>
-  </div>
-  <div className="  ">
-         <ButtonLink
-            value="عرض التفاصيل "
-             color="white"
-            radius="pill"
-            style="outline"
-            href="/project/detail"
-         />
-        </div>
-</div>
-    </div>
-    </div>
-    
-
-  );
-}
+type TypeProps = {
+	title: string;
+	description: string;
+	image: string;
+	alt: string;
+};
+const ProjectCard = (props: TypeProps) => {
+	return (
+		<div className="flex justify-center  p-3   ">
+			<div className="  max-w-sm rounded-3xl overflow-hidden shadow-lg">
+				<img className="w-full" src={props.image} alt={props.alt} />
+				<div className="flex flex-col place-items-center text-center mt-10">
+					<div className="mb-2.5">
+						<div className="font-bold text-xl mb-2 text-primary">{props.title}</div>
+						<p className="text-white text-base">{props.description} </p>
+					</div>
+					<div className="  ">
+						<ButtonLink
+							value="عرض التفاصيل "
+							color="white"
+							radius="pill"
+							style="outline"
+							href="/project/detail"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default ProjectCard;
